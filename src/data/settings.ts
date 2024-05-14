e19904197c441ef9049c6e6f34fdacbb3c9c40c9
nnbveh <nnbveh@gmail.com>
2024-05-14 07:11:24 +0700

    yep

--- src/data/settings.ts
+++
@@ -1,7 +0,0 @@
-export const settings = {
-  site: 'https://simple-blog.littlesticks.dev',
-  name: 'Simple Blog',
-  title: 'Site title',
-  description: 'Site description',
-  showLittleSticksPlug: true,
-}
